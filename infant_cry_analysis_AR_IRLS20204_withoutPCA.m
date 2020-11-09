bfc73da0d5b7447cb0c89a53f3cd1347d242f1b4
knights207210 <xuha2442@colorado.edu>
2020-11-09 01:25:02 -0700

    continuous variables

--- infant_cry_analysis_AR_IRLS20204_withoutPCA.m
+++ infant_cry_analysis_AR_IRLS20204_withoutPCA.m
@@ -5,9 +5,11 @@
 % 5. ROI analysis
 % 6. Haven't done ANCOVA yet, need to ask about ANCOVA first
 % 7. raw.probe.defaultdrawfcn='10-20';
+% 8. find(conStats.ttest.p<0.05)
 %figure
 %raw.probe.draw
 %raw.probe.defaultdrawfcn='3D Mesh (top)'; 
+% nirs.io.loadNIRx
 %figure
 %raw.probe.draw
 clear;
@@ -17,9 +19,24 @@ clc;
 group_number = 2;
 
 %% load data
+%root_dir = '/Users/hanxu/Downloads/fnirs_test'
+%raw = nirs.io.loadDirectory(root_dir, {'group', 'subject'});
+%raw = nirs.io.loadNIRx(root_dir)
+
+%for visualization
+%root_dir = '/Users/hanxu/Downloads/prelim_data/5084/2019-05-10_002';  % The path here needs to be the path where it stores the
+%source files of fnirs data, rather than the coverted .nirs
+%raw = nirs.io.loadNIRx(root_dir);
+%raw.probe.defaultdrawfcn='3D Mesh (top)';
+%raw.probe.draw
+
 root_dir = '/Users/hanxu/Downloads/fnirs_test'
 raw = nirs.io.loadDirectory(root_dir, {'group', 'subject'});
 
+%% Create demographics table
+
+demographics = nirs.createDemographicsTable(raw);
+disp(demographics)
 %% Space Registration
 % add an anchor to attach the probe to the center of the forehead (FpZ)
 % TODO: The following code cannot be used for further visualizations yet.
@@ -70,8 +87,8 @@ for s = 1:size(raw)
     
 % Change stimuli duration
     %raw(s) = nirs.viz.StimUtil(raw(s));
-    raw(s)=nirs.design.change_stimulus_duration(raw(s),'stim_channel6',10);
-    raw(s)=nirs.design.change_stimulus_duration(raw(s),'stim_channel7',10);
+    raw(s)=nirs.design.change_stimulus_duration(raw(s),'channel_6',10);
+    raw(s)=nirs.design.change_stimulus_duration(raw(s),'channel_7',10);
     %raw(s).gui
 
 % turncate for quality checking
@@ -82,11 +99,11 @@ for s = 1:size(raw)
     val6 = 6;
     val7 = 7;
     for a = 1:n
-        if strcmp(data.stimulus.keys{1,a}, 'stim_channel6')
+        if strcmp(data.stimulus.keys{1,a}, 'channel_6')
             val6 = a;
         end
         %if data.stimulus.keys{1,a} == 'stim_channel11'
-        if strcmp(data.stimulus.keys{1,a}, 'stim_channel7')
+        if strcmp(data.stimulus.keys{1,a}, 'channel_7')
             val7 = a;
             break;
         end
@@ -130,13 +147,13 @@ j = nirs.modules.RemoveStimless();
 % task being analyzed)
 
 j = nirs.modules.KeepStims( j);
-j.listOfStims = {'stim_channel6', 'stim_channel7'};  % take a look at the raw data containing condition information, to see whether we can fix the condition information
+j.listOfStims = {'channel_6', 'channel_7'};  % take a look at the raw data containing condition information, to see whether we can fix the condition information
 
 % rename stims
 j = nirs.modules.RenameStims( j );
 j.listOfChanges = {
-    'stim_channel6', 'infantCry'; 
-    'stim_channel7', 'infantNoise'};
+    'channel_6', 'infantCry'; 
+    'channel_7', 'infantNoise'};
 
 % resample to 4Hz
 j = nirs.modules.Resample( j );
@@ -208,27 +225,45 @@ j.trend_func = @(t) nirs.design.trend.dctmtx(t, 0.08);
 %Canonical basis function
 j.basis = Dictionary();
 j.basis('default') = nirs.design.basis.Canonical(); 
-j.basis('stim_channel6') = nirs.design.basis.Canonical();
-j.basis('stim_channel7') = nirs.design.basis.Canonical();
+j.basis('infantCry') = nirs.design.basis.Canonical();
+j.basis('infantNoise') = nirs.design.basis.Canonical();
 SubjStats = j.run( hb );
 %% the parameters can be modified in nirs.design.basis.Canonical
 %% save GLM model
 mkdir /Users/hanxu/Downloads/fnirs_test GLM
 for s = 1:size(transpose(SubjStats))
     writetable(SubjStats(s).table,[root_dir,'/GLM/sub',num2str(s),'.txt'], 'Delimiter', ' '); 
-    writetable(SubjStats(s).table,[root_dir,'/GLM/sub',num2str(s),'.txt'], 'Delimiter', ' '); 
+    writetable(SubjStats(s).table,[root_dir,'/GLM/sub',num2str(s),'.xls'], 'Delimiter', ' ');
 end
 
+%% Add demographic information
+j_demo = nirs.modules.AddDemographics();
+j_demo.demoTable = readtable( [root_dir '/demographics.csv'] );
+
+% We are going to match the subject column in the above table
+j_demo.varToMatch = 'subject';  
+% Note "subject" (or whatever you are matching based on) needs to be an
+% entry in BOTH the CSV file and the nirs.core.<> class that you are
+% loading the demographics to.  
+
+% This is a job, so we need to run it like the others.  In this case, let's
+% run the job on the SubjStats variable we just created.  We could, of
+% course, have ran this job earlier on the raw data
+SubjStats = j_demo.run(SubjStats);
+
+% Just like before, we can check the demographics by using
+disp(nirs.createDemographicsTable( SubjStats ));
 %% data view on individual level
 mkdir /Users/hanxu/Downloads/fnirs_test data_individual
 for s = 1:size(transpose(SubjStats))
     %SubjStats(s).probe.defaultdrawfcn='3D mesh'/'10-20';  % cannot work, error message 'No public field defaultdrawfcn exists for class nirs.core.Probe.'
+    SubjStats(s).probe.defaultdrawfcn='3D mesh';
     SubjStats(s).draw('tstat', [], 'p < 0.05');
     for i = 1:4
         saveas(gcf,[root_dir,'/data_individual/sub',num2str(s),'_', num2str(i),'.png'])
         close;
     end
-    writetable(SubjStats(s).table,[root_dir,'/data_individual/SubjStatssub',num2str(s),'.xls'], 'Delimiter', ' ');
+    %writetable(SubjStats(s).table,[root_dir,'/data_individual/SubjStatssub',num2str(s),'.xls'], 'Delimiter', ' ');
 end
 
 %% contrast on individual level and ROI
@@ -241,11 +276,13 @@ for s = 1:size(transpose(SubjStats))
     ContrastStats = SubjStats(s).ttest(c);
     % Display the contrasts
     ContrastStats.draw('tstat', [], 'p < 0.05');
+    
     for i = 1:2
         saveas(gcf,[root_dir,'/contrast_individual/sub',num2str(s),'_', num2str(i),'.png'])
         close;
     end
-    writetable(ContrastStats.table,[root_dir,'/contrast_individual/ContrastStatssub',num2str(s),'.xls'], 'Delimiter', ' ');
+    %writetable(ContrastStats.table,[root_dir,'/contrast_individual/ContrastStatssub',num2str(s),'.xls'], 'Delimiter', ' ');
+
 %% ROI Analysis Individual
 %Src - Det to include in the ROI, need to be manually defined by ourself,
 %below is just sample Src-Det set
@@ -271,11 +308,12 @@ for s = 1:size(transpose(SubjStats))
 
     
     dataROI.draw('tstat', [], 'p < 0.05');
+    
     for i = 1:2
         saveas(gcf,[root_dir,'/data_individual_ROI/sub',num2str(s),'_', num2str(i),'.png'])
         close;
     end
-    writetable(dataROI.table,[root_dir,'/data_individual_ROI/dataROIsub',num2str(s),'.xls'], 'Delimiter', ' ');
+    %writetable(dataROI.table,[root_dir,'/data_individual_ROI/dataROIsub',num2str(s),'.xls'], 'Delimiter', ' ');
 end
 
 %% Group Analysis
@@ -287,53 +325,60 @@ SubjStats = job.run(SubjStats);
 %% group models
 % Uses Wilkinson notation for specifying models
 job = nirs.modules.MixedEffects();
-job.formula = 'beta ~ -1 + group:cond + (1|subject)';
+job.formula = 'beta ~ -1 + group:cond + demo1 + (1|subject)';
 % this would model the two conditions (A and B) per group treating subject as a random variable.
+%job.dummyCoding = 'full';
 job.dummyCoding = 'full';
 job.include_diagnostics=true;
-GroupStats = job.run(SubjStats);
+GroupStats_ME = job.run(SubjStats);
 %%%job=nirs.modules.AnovaN;
 %%%job.variables={'cond','group'};
 %%%GroupStats = job.run(SubjStats);
 
 mkdir /Users/hanxu/Downloads/fnirs_test group_stats
-save([root_dir,'/group_stats/group_stats.mat'],'GroupStats');
+save([root_dir,'/group_stats/group_stats.mat'],'GroupStats_ME');
 
 %% Visualize the group level stats
 mkdir /Users/hanxu/Downloads/fnirs_test data_group
-GroupStats.draw('tstat', [-10 10], 'p < 0.05')
-for i = 1:4*group_number
+GroupStats_ME.draw('tstat', [-10 10], 'p < 0.05')
+
+for i = 1:5*group_number
     saveas(gcf,[root_dir,'/data_group/',num2str(i),'.png'])
     close;
 end
-writetable(GroupStats.table,[root_dir,'/data_group/GroupStats','.xls'], 'Delimiter', ' ');
+%writetable(GroupStats.table,[root_dir,'/data_group/GroupStats','.xls'], 'Delimiter', ' ');
+
 %% FDR corrected (q < 0.05)
 mkdir /Users/hanxu/Downloads/fnirs_test data_FDRcorrected_group
-GroupStats.draw('tstat', [-5 5], 'q < 0.05')
-for i = 1:4*group_number
+GroupStats_ME.draw('tstat', [-5 5], 'q < 0.05')
+
+for i = 1:5*group_number
     saveas(gcf,[root_dir,'/data_FDRcorrected_group/',num2str(i),'.png'])
     close;
 end
 
-%% group contrast
+%% group contrast -- compare between two conditions or groups
 %% display condition
-disp(GroupStats.conditions);
+disp(GroupStats_ME.conditions);
 %% run contrast
-c = [eye(4);  % all 5 of the original variables
-     1 0 -1 0; % X - Y for group 1
-     0 1 0 -1; % X - Y for group 2
-     1 -1 0 0; % G1 - G2 for X
-     0 0 1 -1]; % G1 - G2 for Y
-%c = [1 -1 0 0]
+%c = [eye(4);  % all 5 of the original variables
+     %1 0 -1 0; % X - Y for group 1
+     %0 1 0 -1; % X - Y for group 2
+     %1 -1 0 0; % G1 - G2 for X
+     %0 0 1 -1]; % G1 - G2 for Y
+c = [0 1 -1 0 0] % need to adjust 'c' based on how many covariates are we going to include
 size_c = size(c);
-ContrastStats = GroupStats.ttest(c);
-ContrastStats.draw('tstat', [-5 5], 'p < 0.05');
 mkdir /Users/hanxu/Downloads/fnirs_test contrast_group
+ContrastStats = GroupStats_ME.ttest(c);
+ContrastStats.draw('tstat', [-5 5], 'p < 0.05');
+
+
+
 for i = 1:2*size_c(1)
     saveas(gcf,[root_dir,'/contrast_group/',num2str(i),'.png'])
     close;
 end
-writetable(ContrastStats.table,[root_dir,'/contrast_group/ContrastStats','.xls'], 'Delimiter', ' ');
+%writetable(ContrastStats.table,[root_dir,'/contrast_group/ContrastStats','.xls'], 'Delimiter', ' ');
 %% FDR corrected (q < 0.05)
 ContrastStats.draw('tstat', [-5 5], 'q < 0.05');
 mkdir /Users/hanxu/Downloads/fnirs_test contrast_FDRcorrected_group
@@ -367,9 +412,36 @@ mkdir /Users/hanxu/Downloads/fnirs_test data_group_ROI
 for s = 1:size(dataROI)
     dataROI(s).draw('tstat', [], 'p < 0.05');
     for i = 1:2*size_c(1)
-        saveas(gcf,[root_dir,'/data_group_ROI/sub',num2str(s),'_', num2str(i),'.png'])
+        saveas(gcf,[root_dir,'/data_group_ROI/',num2str(s),'_', num2str(i),'.png'])
         close;
     end
-    writetable(dataROI(s).table,[root_dir,'/data_group_ROI/dataROI',num2str(s), '.xls'], 'Delimiter', ' ');
+    %writetable(dataROI(s).table,[root_dir,'/data_group_ROI/dataROI',num2str(s), '.xls'], 'Delimiter', ' ');
 end
 
+
+%% group contrast -- compare among multiple variables - using ANCOVA
+job = nirs.modules.Anova();
+job.formula = 'beta ~ group*cond + demo1 + (1|subject)';
+job.dummyCoding = 'effects';
+GroupStats_ANOVA = job.run(SubjStats);
+
+mkdir /Users/hanxu/Downloads/fnirs_test group_stats_ANOVA
+save([root_dir,'/group_stats_ANOVA/group_stats_ANOVA.mat'],'GroupStats_ANOVA');
+%% Visualize the group level stats - ANOVA
+mkdir /Users/hanxu/Downloads/fnirs_test group_ANOVA
+GroupStats_ANOVA.draw(100, 'p < 0.05')
+
+for i = 1:5*group_number
+    saveas(gcf,[root_dir,'/group_ANOVA/',num2str(i),'.png'])
+    close;
+end
+%writetable(GroupStats.table,[root_dir,'/data_group/GroupStats','.xls'], 'Delimiter', ' ');
+
+%% FDR corrected (q < 0.05) - ANOVA
+mkdir /Users/hanxu/Downloads/fnirs_test FDRcorrected_group_ANOVA
+GroupStats_ANOVA.draw(100, 'q < 0.05')
+
+for i = 1:5*group_number
+    saveas(gcf,[root_dir,'/FDRcorrected_group_ANOVA/',num2str(i),'.png'])
+    close;
+end
